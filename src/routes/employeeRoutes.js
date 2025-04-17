87193f293f621bdad9a35bb7f1582219842e62e7
Rodrineitor <149251680+740-224-18@users.noreply.github.com>
2025-04-17 00:03:27 -0400

    Update employeeRoutes.js

--- src/routes/employeeRoutes.js
+++ src/routes/employeeRoutes.js
@@ -30,14 +30,4 @@ router.get('/edit/:id', showEditForm);
 router.post('/edit/:id', upload.single('foto'), updateEmployee);
 router.get('/delete/:id', deleteEmployee);
 
-
-// Rutas para empleados
-router.get('/employees', adminController.listEmployees);        // Ver empleados
-router.get('/employees/add', adminController.addEmployeeForm);  // Formulario de agregar empleado
-router.post('/employees', adminController.addEmployee);         // Agregar empleado
-router.get('/employees/edit/:id', adminController.editEmployeeForm); // Formulario para editar empleado
-router.post('/employees/edit/:id', adminController.updateEmployee); // Actualizar empleado
-router.get('/employees/delete/:id', adminController.deleteEmployee); // Eliminar empleado
-
-
 module.exports = router;
