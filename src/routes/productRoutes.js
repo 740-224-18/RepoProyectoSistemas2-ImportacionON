86a515648ed9aa51e8b2b674ee51330e156a7ca5
Rodrineitor <149251680+740-224-18@users.noreply.github.com>
2025-04-17 00:04:43 -0400

    Update productRoutes.js

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-
 const multer = require('multer');
 const path = require('path');
 const {
@@ -26,15 +25,5 @@ router.post('/add', upload.single('imagen'), saveProduct);
 router.get('/edit/:id', showEditForm);
 router.post('/edit/:id', upload.single('imagen'), updateProduct);
 router.get('/delete/:id', deleteProduct);
-
-
-// Rutas para productos
-router.get('/products', adminController.listProducts);          // Ver productos
-router.get('/products/add', adminController.addProductForm);    // Formulario de agregar producto
-router.post('/products', adminController.addProduct);           // Agregar producto
-router.get('/products/edit/:id', adminController.editProductForm); // Formulario para editar producto
-router.post('/products/edit/:id', adminController.updateProduct); // Actualizar producto
-router.get('/products/delete/:id', adminController.deleteProduct); // Eliminar producto
-
 
 module.exports = router;
